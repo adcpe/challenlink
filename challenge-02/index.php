25cedfd2ad951e0919f847eefa92d7ce66b9aeab
Andrés Del Carpio <andresdc@andresdc.net>
2020-12-15 16:58:30 -0500

    Added Solution 2

--- challenge-02/index.php
+++ challenge-02/index.php
@@ -2,9 +2,37 @@
 
 function noIterate($strArr)
 {
-    // code goes here
-    return $strArr;
+    $strLength = strlen($strArr[1]);
+    $maxIndex = strlen($strArr[0]);
+
+    while ($strLength <= $maxIndex)
+    {
+        for ($i=0; $i <= $strLength ; $i++)
+        { 
+            $origSample = str_split($strArr[1]);
+            $newSample = str_split(substr($strArr[0], $i, $strLength));
+            $newSampleCopy = implode($newSample);
+            $hits = 0;
+
+            foreach ($origSample as $val)
+            {
+               if (in_array($val, $newSample))
+               {
+                   array_splice($newSample, array_search($val, $newSample), 1);
+                   $hits += 1;
+               }
+
+            }
+            if (count($origSample) == $hits) return $newSampleCopy;
+        }
+        $strLength += 1;
+    }
+    return "hey";
 }
 
 // keep this function call here
-echo noIterate(["ahffaksfajeeubsne", "jefaa"]);
+echo noIterate(["ahffaksfajeeubsne", "jefaa"]); // "aksfaje"
+echo "\r\n";
+echo noIterate(["aksfaje", "jefaa"]); // "aksfaje"
+echo "\r\n";
+echo noIterate(["aaffhkksemckelloe", "fhea"]); # "affhkkse"
