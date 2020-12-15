d1973e112f3a6ee0bdb916540349f7daae2f04b4
Andrés Del Carpio <andresdc@andresdc.net>
2020-12-15 13:25:17 -0500

    Added Solution 1

--- challenge-01/index.php
+++ challenge-01/index.php
@@ -1,10 +1,17 @@
 <?php
 
-function findPoint($strArr)
+function splitStr($str)
 {
-    // code goes here
-    return $strArr;
+    return preg_split("/, /", $str);
 }
 
-// keep this function call here
-echo findPoint(['1, 3, 4, 7, 13', '1, 2, 4, 13, 15']);
+function findPoint($strArr)
+{
+    $newArr = array_map ("splitStr", $strArr);
+    return implode(",", array_intersect($newArr[0], $newArr[1]));
+}
+
+echo findPoint(['1, 3, 4, 7, 13', '1, 2, 4, 13, 15']); // "1,4,13"
+echo "\r\n";
+echo findPoint(['1, 3, 9, 10, 17, 18', '1, 4, 9, 10']); // "1,9,10"
+
