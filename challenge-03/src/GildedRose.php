dfdc48d7d02bfd0b76ce6100b9e6ce70380d9dd3
Andrés Del Carpio <andresdc@andresdc.net>
2020-12-16 18:39:57 -0500

    Added Solution 3

--- challenge-03/src/GildedRose.php
+++ challenge-03/src/GildedRose.php
@@ -2,16 +2,12 @@
 
 namespace App;
 
-class GildedRose
-{
+class GildedRose {
     public $name;
-
     public $quality;
-
     public $sellIn;
 
-    public function __construct($name, $quality, $sellIn)
-    {
+    public function __construct($name, $quality, $sellIn) {
         $this->name = $name;
         $this->quality = $quality;
         $this->sellIn = $sellIn;
@@ -21,53 +17,47 @@ class GildedRose
         return new static($name, $quality, $sellIn);
     }
 
-    public function tick()
-    {
-        if ($this->name != 'Aged Brie' and $this->name != 'Backstage passes to a TAFKAL80ETC concert') {
-            if ($this->quality > 0) {
-                if ($this->name != 'Sulfuras, Hand of Ragnaros') {
-                    $this->quality = $this->quality - 1;
-                }
-            }
-        } else {
-            if ($this->quality < 50) {
-                $this->quality = $this->quality + 1;
-
-                if ($this->name == 'Backstage passes to a TAFKAL80ETC concert') {
-                    if ($this->sellIn < 11) {
-                        if ($this->quality < 50) {
-                            $this->quality = $this->quality + 1;
-                        }
-                    }
-                    if ($this->sellIn < 6) {
-                        if ($this->quality < 50) {
-                            $this->quality = $this->quality + 1;
-                        }
-                    }
-                }
-            }
-        }
-
+    public function tick() {
+        // update sellIn
         if ($this->name != 'Sulfuras, Hand of Ragnaros') {
             $this->sellIn = $this->sellIn - 1;
         }
 
-        if ($this->sellIn < 0) {
-            if ($this->name != 'Aged Brie') {
-                if ($this->name != 'Backstage passes to a TAFKAL80ETC concert') {
-                    if ($this->quality > 0) {
-                        if ($this->name != 'Sulfuras, Hand of Ragnaros') {
-                            $this->quality = $this->quality - 1;
-                        }
-                    }
-                } else {
-                    $this->quality = $this->quality - $this->quality;
-                }
-            } else {
-                if ($this->quality < 50) {
-                    $this->quality = $this->quality + 1;
-                }
+        // update quality
+        // Aged Brie
+        if ($this->name == 'Aged Brie') {
+            $this->quality = $this->quality + 1;
+            if ($this->sellIn <= 0) $this->quality = $this->quality + 1;
+        // Backstage passes to a TAFKAL80ETC concert
+        } elseif ($this->name == 'Backstage passes to a TAFKAL80ETC concert') {
+            if ($this->sellIn >= 10) {
+                $this->quality = $this->quality + 1;
+            } elseif ($this->sellIn < 10 and $this->sellIn > 5) {
+                $this->quality = $this->quality + 2;
+            } elseif ($this->sellIn <= 5 and $this->sellIn >= 0) {
+                $this->quality = $this->quality + 3;
+            } elseif ($this->sellIn < 0) {
+                $this->quality = 0;
             }
+        // Normal
+        } elseif ($this->name == 'normal') {
+            if ($this->sellIn >= 0) {
+                $this->quality = $this->quality - 1;
+            } else {
+                $this->quality = $this->quality - 2;
+            }
+        // Conjured Mana Cake
+        } elseif ($this->name == 'Conjured Mana Cake') {
+            $this->quality = $this->quality - 2;
+            if ($this->sellIn < 0) $this->quality = $this->quality - 2;
+        }
+
+        // don't let quality be lower than than 0
+        if ($this->quality < 0) $this->quality = 0;
+
+        // don't let quality be higher than 50 unless it's Sulfuras
+        if ($this->quality > 50 and $this->name != 'Sulfuras, Hand of Ragnaros') {
+            $this->quality = 50;
         }
     }
 }
