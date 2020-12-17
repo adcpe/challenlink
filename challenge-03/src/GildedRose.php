c51c522aaaefff36f5a3deedbf185b04bc5e699a
Andrés Del Carpio <andresdc@andresdc.net>
2020-12-16 22:39:02 -0500

    Refactored some conditionals into ternaries

--- challenge-03/src/GildedRose.php
+++ challenge-03/src/GildedRose.php
@@ -19,37 +19,29 @@ class GildedRose {
 
     public function tick() {
         // update sellIn
-        if ($this->name != 'Sulfuras, Hand of Ragnaros') {
-            $this->sellIn = $this->sellIn - 1;
-        }
+        if ($this->name != 'Sulfuras, Hand of Ragnaros') $this->sellIn -= 1;
 
         // update quality
         // Aged Brie
         if ($this->name == 'Aged Brie') {
-            $this->quality = $this->quality + 1;
-            if ($this->sellIn <= 0) $this->quality = $this->quality + 1;
+            $this->quality += $this->sellIn > 0 ? 1 : 2;
         // Backstage passes to a TAFKAL80ETC concert
         } elseif ($this->name == 'Backstage passes to a TAFKAL80ETC concert') {
             if ($this->sellIn >= 10) {
-                $this->quality = $this->quality + 1;
+                $this->quality += 1;
             } elseif ($this->sellIn < 10 and $this->sellIn > 5) {
-                $this->quality = $this->quality + 2;
+                $this->quality += 2;
             } elseif ($this->sellIn <= 5 and $this->sellIn >= 0) {
-                $this->quality = $this->quality + 3;
+                $this->quality += 3;
             } elseif ($this->sellIn < 0) {
                 $this->quality = 0;
             }
         // Normal
         } elseif ($this->name == 'normal') {
-            if ($this->sellIn >= 0) {
-                $this->quality = $this->quality - 1;
-            } else {
-                $this->quality = $this->quality - 2;
-            }
+            $this->quality -= $this->sellIn >= 0 ? 1 : 2;
         // Conjured Mana Cake
         } elseif ($this->name == 'Conjured Mana Cake') {
-            $this->quality = $this->quality - 2;
-            if ($this->sellIn < 0) $this->quality = $this->quality - 2;
+            $this->quality -= $this->sellIn > 0 ? 2 : 4;
         }
 
         // don't let quality be lower than than 0
